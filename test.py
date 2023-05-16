622b0c7fe21820de9edc9e648b285ca9298d47c4
truong571 <114497275+truong571@users.noreply.github.com>
2023-05-16 19:59:02 +0700

    Update test.py

--- test.py
+++ test.py
@@ -24,7 +24,6 @@ def predict(test_doc):
     return False
 
 
-"""
 import time
 start_time = time.time()
 test_doc = '''Chỉ số BMI là gì?.'''
@@ -32,14 +31,3 @@ print(predict(test_doc))
 end_time = time.time()
 execution_time = end_time - start_time
 print("Thời gian thực thi: ", execution_time, " giây")
-
-
-đoạn này viết thêm để chạy tính thời gian khi bỏ vào project thì xoá đi 
-
-cách chạy anh cứ gôm hết các import anh bỏ vào gg colab cái nào báo lỗi 
-chưa pip install anh copy về vs code anh install là chạy được. 
-xong rồi anh chạy file train.py để lưu X_data.pkl và y_data.pkl thật ra anh không 
-cần chạy file train.py cũng được chỉ cần có X_data.pkl và y_data.pkl là chạy được file test.py rồi
-cái train.py chỉ chạy để lưu 2 fiel kia thôi chứ không liên quan gì tới file test.py.
-hàm dự đoán nếu là sức khoẻ thì true mà không thì false.
-"""
